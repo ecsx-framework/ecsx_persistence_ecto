d91e849fbb0b771bd32b42fb7810f6776e75a025
APB9785 <74077809+APB9785@users.noreply.github.com>
2024-02-05 16:44:44 -0600

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule EcsxPersistenceEcto.MixProject do
   use Mix.Project
 
   @gh_url "https://github.com/TheFirstAvenger/ecsx_persistence_ecto"
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [
